d28d2a72e8825b56e3112603cfed0a2924364f91
zhengzhanghai <zzh_xiaohai@163.com>
2020-05-14 18:30:55 +0800

    添加一些排序算法

--- DataStructureAndAlgorithm/DataStructureAndAlgorithm/Contents/Sort/SolutionSort.swift
+++ DataStructureAndAlgorithm/DataStructureAndAlgorithm/Contents/Sort/SolutionSort.swift
@@ -10,13 +10,18 @@ import UIKit
 
 /// 排序
 class SolutionSort {
+    var len: Int = 0
+    
     func test() {
-        var array = [5,7,3,1,9,2,4,6,8]
+        var array = [5,7,1,9,2,4,6,8,7]
 //        bubbleSort(&array)
 //        selectSort(&array)
 //        insertSort(&array)
 //        shellSort(&array)
-        quickSort(&array)
+//        quickSort(&array)
+//        heapSort(&array)
+//        countSort(&array)
+        bucketSort(&array)
         print(array)
     }
 }
@@ -137,18 +142,124 @@ extension SolutionSort {
 /// 堆排序
 extension SolutionSort {
     
-    /// 建立大项堆
-    func buildMaxHeap(_ nums: [Int]) {
-        
+    /// 建立小根堆
+    func buildMaxHeap(_ nums: inout [Int]) {
+        len = nums.count
+        var i = Int(floor(Double(nums.count) / 2))
+        while i >= 0 {
+            heapify(&nums, i)
+            i -= 1
+        }
     }
     
     /// 堆调整
-    func heapify(_ nums: inout [Int]) {
+    func heapify(_ nums: inout [Int], _ i: Int) {
+        let leftIndex = 2 * i + 1
+        let rightIndex = 2 * i + 2
+        var maxIndex = i
         
+        if leftIndex < len && nums[leftIndex] > nums[maxIndex] {
+            maxIndex = leftIndex
+        }
+        if rightIndex < len && nums[rightIndex] > nums[maxIndex] {
+            maxIndex = rightIndex
+        }
+        
+        if i != maxIndex {
+            swap(&nums, i, maxIndex)
+            // 单纯建堆来说，这个递归是没必要的，这儿是为了后面，交换元素后的堆调整
+            heapify(&nums, maxIndex)
+        }
+    }
+    
+    /// 交换
+    func swap(_ nums: inout [Int], _ i: Int, _ j: Int) {
+        (nums[i], nums[j]) = (nums[j], nums[i])
     }
     
     func heapSort(_ nums: inout [Int]) {
-        
+        buildMaxHeap(&nums)
+        var index = nums.count - 1
+        while index > 0 {
+            swap(&nums, 0, index)
+            len -= 1
+            heapify(&nums, 0)
+            index -= 1
+        }
     }
 }
 
+extension SolutionSort {
+    func countSort(_ nums: inout [Int]) {
+        guard nums.count > 1 else {
+            return
+        }
+        var maxNum = Int.min
+        var hash = [Int: Int]()
+        
+        for num in nums {
+            maxNum = max(maxNum, num)
+            if let count = hash[num] {
+                hash[num] = count + 1
+            } else {
+                hash[num] = 1
+            }
+        }
+                        
+        var i = 0
+        for index in 0 ... maxNum {
+            guard let count = hash[index] else {
+                continue
+            }
+            for _ in 0 ..< count {
+                nums[i] = index
+                i += 1
+            }
+        }
+    }
+}
+
+/// 桶排序
+extension SolutionSort {
+    func bucketSort(_ nums: inout [Int]) {
+        guard !nums.isEmpty else {
+            return
+        }
+        
+        var maxValue = nums[0]
+        var minVlaue = nums[0]
+        
+        for num in nums {
+            maxValue = max(maxValue, num)
+            minVlaue = min(minVlaue, num)
+        }
+        
+        let bucketSize = 30
+        let bucketCount = (maxValue - minVlaue) / bucketSize + 1
+        
+        var buckets = [[Int]](repeating: [], count: bucketCount)
+        
+        // 将数分配到桶里
+        for index in 0 ..< nums.count {
+            buckets[(nums[index] - minVlaue) / bucketSize].append(nums[index])
+        }
+        
+        /// 把桶内部的元素进行排序
+        bucketSortInside(&buckets)
+        
+        var index = 0
+        for i in 0 ..< buckets.count {
+            for j in 0 ..< buckets[i].count {
+                nums[index] = buckets[i][j]
+                index += 1
+            }
+        }
+    }
+    
+    ///
+    func bucketSortInside(_ buckets: inout [[Int]]) {
+        for i in 0 ..< buckets.count {
+            bubbleSort(&buckets[i])
+        }
+    }
+}
