a1b5a0dd37af8ceed7eee300150a5743cf3939f9
zhengzhanghai <zzh_xiaohai@163.com>
2020-05-25 17:25:32 +0800

    添加LRU算法

--- DataStructureAndAlgorithm/DataStructureAndAlgorithm/Controllers/ViewController.swift
+++ DataStructureAndAlgorithm/DataStructureAndAlgorithm/Controllers/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         //        SolutionTwoPointSearch().test()
         //        SolutionBinaryTree().test()
         //        SolutionSort().test()
-        LeetCode5().test()
+        LeetCode146().test()
  
     }
 }
